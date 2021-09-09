0a93c383c6b0e9791560d878335a5fcd26df0cf2
Danielle Rodrigues <livibellyrds@gameil.com>
2021-09-08 21:41:40 -0300

    rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,21 @@
 const { Router } = require('express');
 const usersRouter = require('./usersRoutes');
 const storesRouter = require('./storesRoutes');
+const shoppingsRouter = require('./shoppingsRoutes');
+const productsRouter = require('./productsRoutes');
+const ordersRouter = require('./ordersRoutes');
+
+
 
 const router = Router();
 
 router.use('/users', usersRouter);
 router.use('/stores', storesRouter);
+router.use('/shoppings', shoppingsRouter);
+router.use('/products', productsRouter);
+router.use('/orders',ordersRouter);
+
+
+
 
 module.exports = router;
